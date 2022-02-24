f7010d950b52b6f9b794c4c542df473a784cd604
Gábor Csárdi <csardi.gabor@gmail.com>
2022-02-24 11:30:58 +0100

    Experimental manual page

--- build.rs
+++ build.rs
@@ -2,8 +2,11 @@
 use clap_complete::shells::Bash;
 use clap_complete::shells::Zsh;
 use clap_complete::shells::PowerShell;
+use clap_mangen::Man;
+
 use std::env;
 use std::io::Error;
+use std::path::Path;
 
 include!("src/args.rs");
 
@@ -24,6 +27,26 @@ fn main() -> Result<(), Error> {
 
     let path = clap_complete::generate_to(PowerShell, &mut app, &name, &outdir);
     println!("powershell completion file is generated: {:?}", path);
+
+    #[cfg(any(target_os = "macos", target_os = "linux"))]
+    {
+        let app = app
+            .name("rim")
+            .about("R Installation Manager")
+            .long_about(HELP_ABOUT);
+        let man = clap_mangen::Man::new(app);
+        let mut buffer: Vec<u8> = Default::default();
+        man.render_title(&mut buffer)?;
+        man.render_name_section(&mut buffer)?;
+        man.render_synopsis_section(&mut buffer)?;
+        man.render_description_section(&mut buffer)?;
+        man.render_options_section(&mut buffer)?;
+        man.render_subcommands_section(&mut buffer)?;
+        man.render_extra_section(&mut buffer)?;
+
+        let outdir = Path::new(&outdir);
+        std::fs::write(outdir.join("rim.1"), buffer)?;
+    }
 
     Ok(())
 }
