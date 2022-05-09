d1a0095c7d094592fbaef23905bb7b85447ec788
Gábor Csárdi <csardi.gabor@gmail.com>
2022-05-09 14:07:50 +0200

    Update choco package to 0.3.0
    [ci skip]

--- choco/rim/tools/chocolateyinstall.ps1
+++ choco/rim/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
-  Url64bit       = 'https://github.com/r-lib/rim/releases/download/v0.2.3/rim-windows-0.2.3.exe'
-  Checksum64     = 'ae03f18e825af8ced9a4d3058f09d9ad6f7e4824901b9e6808d9874a334bdd74'
+  Url64bit       = 'https://github.com/r-lib/rim/releases/download/v0.3.0/rim-windows-0.3.0.exe'
+  Checksum64     = 'dd72690fa9eac10e43e6c7cf96b4387de0fd40cb2456668d7f75f34656d3d3af'
   ChecksumType64 = 'sha256'
 }
 
