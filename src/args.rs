17ed9a6f05db2ad243f437bc175f8fe4f5cc4549
Gábor Csárdi <csardi.gabor@gmail.com>
2022-05-27 11:41:29 +0200

    Add help for `rig library`

--- src/args.rs
+++ src/args.rs
@@ -16,6 +16,27 @@ std::include!("help-windows.in");
 #[cfg(target_os = "linux")]
 std::include!("help-linux.in");
 
+const HELP_LIBRARY: &str = r#"
+DESCRIPTION
+    Manage package libraries [alias: lib] (experimental)
+
+    rig supports multiple user package libraries. The usual user library is
+    called "main".
+
+    `rig library default` shows or sets the default library for the
+     current R version.
+    `rig library list` lists all libraries for the current R version.
+    `rig library add` adds a new library for the current R version.
+    `rig library rm` deletes a library, including all packages in it.
+    It is not possible to delete the current default library, and it is not
+    possible to delete the main library.
+
+    User libraries are implemented at the user level, no administrator or
+    root password is needed to add, set or delete them. If you delete an
+    R installation, the user package libraries and their configurations are
+    kept for all users on the system.
+"#;
+
 pub fn rig_app() -> Command<'static> {
 
     let _arch_x86_64: &'static str = "x86_64";
@@ -362,8 +383,7 @@ pub fn rig_app() -> Command<'static> {
 
 
     let cmd_library = Command::new("library")
-        .about("Manage package libraries [alias: lib] (experimental)")
-        .long_about("TODO")
+        .about(HELP_LIBRARY)
         .aliases(&["lib"])
         .arg_required_else_help(true)
         .arg(
