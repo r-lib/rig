3a3d26be5dc71ca4128e1b06d5f0586ff3043b2d
Gábor Csárdi <csardi.gabor@gmail.com>
2023-08-31 13:09:58 +0200

    Update choco package
    [ci skip]

--- choco/rig/tools/chocolateyinstall.ps1
+++ choco/rig/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
-  Url64bit       = 'https://github.com/r-lib/rig/releases/download/v0.5.3/rig-windows-0.5.3.exe'
-  Checksum64     = 'dfc63ae036fc02ee8719515ba600cdf4d758d4707cdecfb283c65f02f0b07163'
+  Url64bit       = 'https://github.com/r-lib/rig/releases/download/v0.6.0/rig-windows-0.6.0.exe'
+  Checksum64     = '8b75b5a912800e3d0c2ef2470d7633cd8c329d2cf5451c9f8f98dc3a965a6b8e'
   ChecksumType64 = 'sha256'
 }
 
