c98d5f0057059ad13903190b5bd95adcafbe4a6a
Gábor Csárdi <csardi.gabor@gmail.com>
2023-09-09 18:04:23 +0200

    Simplify Docker entry point
    To keep the uid and gid of the files.
    
    [ci skip]

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,25 @@
 #!! /bin/sh
 
-# make sure that there is a group with $LOCAL_GID
+# There is already a rigbuild user and a rigbuild group, we
+# just need to make sure that their uid and gid matches the host.
+apk add shadow
+
+user=`getent passwd $LOCAL_UID | cut -f1 -d:`
+if [ -z "$user" ]; then
+    usermod -u $LOCAL_UID rigbuild
+    user=rigbuild
+fi
 
 group=`getent group $LOCAL_GID | cut -f1 -d:`
 if [ -z "$group" ]; then
-    # if there is a 'rigbuild' group, recreate it with the right gid
-    delgroup rigbuild 2>/dev/null || true
-    addgroup -g "$LOCAL_GID" rigbuild
+    groupmod -g ${LOCAL_GID} rigbuild
     group=rigbuild
 fi
-
-# Recreate rigbuild user with the right UID and GID
-
-deluser rigbuild 2>/dev/null || true
-adduser -u $LOCAL_UID -G $group -D -H rigbuild
 
 # We only need acceess to these and it would takes ~10s to chown all the
 # files of the rust toolchain
 
-chown rigbuild:$group /home/rigbuild/
-chown rigbuild:$group /home/rigbuild/.cargo
+chown $user:$group /home/rigbuild/
+chown $user:$group /home/rigbuild/.cargo
 
-exec su -s /bin/sh rigbuild sh -c "$*"
+exec su -s /bin/sh $user sh -c "$*"
