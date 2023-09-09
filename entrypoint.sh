5589f12fa2c8373f439196c747020c73726a82bf
Gábor Csárdi <csardi.gabor@gmail.com>
2023-09-09 10:56:20 +0200

    Linux build: recreate group with gid
    Otherwise there is a 'rigbuild' group with the
    wrong gid, because adduser also creates a group
    with the same name.
    
    [ci skip]

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,21 +2,17 @@
 
 # make sure that there is a group with $LOCAL_GID
 
-getent group $LOCAL_GID
-cat /etc/passwd
-cat /etc/group
-id -u
-id -g
-
 group=`getent group $LOCAL_GID | cut -f1 -d:`
 if [ -z "$group" ]; then
+    # if there is a 'rigbuild' group, recreate it with the right gid
+    delgroup rigbuild 2>/dev/null || true
     addgroup -g "$LOCAL_GID" rigbuild
     group=rigbuild
 fi
 
 # Recreate rigbuild user with the right UID and GID
 
-deluser rigbuild 2>/dev/null
+deluser rigbuild 2>/dev/null || true
 adduser -u $LOCAL_UID -G $group -D -H rigbuild
 
 # We only need acceess to these and it would takes ~10s to chown all the
