74bb252854431a9630167ad7b6ed2c0b842a0105
Gábor Csárdi <csardi.gabor@gmail.com>
2023-09-09 19:36:45 +0200

    Fix Linux CI

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,22 +1,30 @@
 #!! /bin/sh
 
-# make sure that there is a group with $LOCAL_GID
+set -x
+
+# need a user with $LOCAL_UID
+
+user=`getent passwd $LOCAL_UID | cut -f1 -d:`
+if [ -z "$user" ]; then
+    adduser -D -u "$LOCAL_UID" rig
+    user=rig
+fi
+
+# need a user with $LOCAL_GID
 
 group=`getent group $LOCAL_GID | cut -f1 -d:`
 if [ -z "$group" ]; then
-    addgroup -g "$LOCAL_GID" rigbuild
-    group=rigbuild
+    addgroup -g "$LOCAL_GID" rig
+    group=rig
 fi
-
-# Recreate rigbuild user with the right UID and GID
-
-deluser rigbuild 2>/dev/null
-adduser -u $LOCAL_UID -G $group -D -H rigbuild
 
 # We only need acceess to these and it would takes ~10s to chown all the
 # files of the rust toolchain
 
-chown rigbuild:$group /home/rigbuild/
-chown rigbuild:$group /home/rigbuild/.cargo
+rm -rf /home/$user
+mv /home/rigbuild /home/$user
 
-exec su -s /bin/sh rigbuild sh -c "$*"
+chown $user:$group /home/rig
+chown $user:$group /home/rig/.cargo
+
+exec su -s /bin/sh $user sh -l -c "cd /work && $*"
