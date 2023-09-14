08de95a6c338d9afa94a4286153a7d540184d725
Tymoteusz Makowski <tymek.makowski@gmail.com>
2023-09-14 21:38:51 +0200

    feat: add fish completions

--- build.rs
+++ build.rs
@@ -1,4 +1,5 @@
 use clap_complete::shells::Bash;
+use clap_complete::shells::Fish;
 use clap_complete::shells::PowerShell;
 use clap_complete::shells::Zsh;
 use std::env;
@@ -28,6 +29,9 @@ fn main() -> Result<(), Error> {
 
     let path = clap_complete::generate_to(PowerShell, &mut app, &name, &outdir);
     println!("powershell completion file is generated: {:?}", path);
+
+    let path = clap_complete::generate_to(Fish, &mut app, &name, &outdir);
+    println!("fish completion file is generated: {:?}", path);
 
     Ok(())
 }
