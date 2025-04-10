de3945dde1f2d3cf72a4e3ab3e3bf93289531b1e
Gábor Csárdi <csardi.gabor@gmail.com>
2025-04-10 22:20:07 +0200

    Choco package update to 0.7.0

--- choco/rig/tools/chocolateyinstall.ps1
+++ choco/rig/tools/chocolateyinstall.ps1
@@ -6,8 +6,8 @@ $packageArgs = @{
   PackageName    = $env:ChocolateyPackageName
   FileType       = 'exe'
   SilentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES'
-  Url64bit       = 'https://github.com/r-lib/rig/releases/download/v0.6.0/rig-windows-0.6.0.exe'
-  Checksum64     = '8b75b5a912800e3d0c2ef2470d7633cd8c329d2cf5451c9f8f98dc3a965a6b8e'
+  Url64bit       = 'https://github.com/r-lib/rig/releases/download/v0.7.0/rig-windows-0.7.0.exe'
+  Checksum64     = '9e4d8bae8ce820d25b9a1f49a54ce9c2fa732bcccd47920f49d714f21beb1e02'
   ChecksumType64 = 'sha256'
 }
 
