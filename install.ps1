a091fb765c35d2877f8ee928c7e7499748d00f38
gaborcsardi <csardi.gabor@gmail.com>
2026-08-01 10:30:16 +0000

    Deploying to website from @ r-lib/rig@cfc97ef45dfcdaf830fd8a6dd77edad51e1eec9e 🚀

--- install.ps1
+++ install.ps1
@@ -20,7 +20,7 @@
     (or $env:RIG_PREFIX if set).
 
 .PARAMETER Version
-    Version to install, e.g. 0.10.0. Default: 0.10.0-alpha
+    Version to install, e.g. 0.10.0. Default: 0.10.0-alpha2
     (or $env:RIG_VERSION if set).
 
 .PARAMETER NoModifyPath
@@ -29,7 +29,7 @@
 [CmdletBinding()]
 param(
     [string] $Prefix       = $(if ($env:RIG_PREFIX) { $env:RIG_PREFIX } else { "$env:USERPROFILE\.local" }),
-    [string] $Version      = $(if ($env:RIG_VERSION) { $env:RIG_VERSION } else { "0.10.0-alpha" }),
+    [string] $Version      = $(if ($env:RIG_VERSION) { $env:RIG_VERSION } else { "0.10.0-alpha2" }),
     [switch] $NoModifyPath
 )
 
